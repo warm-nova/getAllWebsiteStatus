925fd07cfbeecba7a431bdf1c6f56f5d5121e8cf
lidecai <lidecai@360.cn>
2015-12-11 16:52:13 +0800

    modify system aceive

--- src/getAllWebsiteStatus/MainProg.java
+++ src/getAllWebsiteStatus/MainProg.java
@@ -1,11 +1,44 @@
 package getAllWebsiteStatus;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class MainProg {
+
+	static HttpURLConnection urlconnection;
+
+	public static int isWebsiteCanAccess(String netAddr) {
+		int count = 0;
+		int returnvalue = 0;
+		while (count < 3) {
+			try {
+				URL url = new URL(netAddr + "/");
+				urlconnection = (HttpURLConnection) url.openConnection();
+				int returncode = urlconnection.getResponseCode();
+
+				if (returncode == 404) {
+					urlconnection.disconnect();
+					returnvalue = 404;
+				} else {
+					returnvalue = returncode;
+					urlconnection.disconnect();
+					break;
+				}
+
+			} catch (Exception e) {
+				returnvalue = -1;
+				continue;
+			}
+			count++;
+		}
+		return returnvalue;
+	}
+
 	public static void main(String args[]) {
 		try {
 			Document document = Jsoup.connect("http://h5.mse.360.cn")
@@ -21,7 +54,10 @@ public class MainProg {
 			Elements imports = document.select("link[href]");
 
 			for (Element link : links) {
-				System.out.println(link.attr("abs:href") + "     " + link.text());
+				String linkaddr = link.attr("abs:href");
+				if (linkaddr.contains("haosou") || linkaddr.contains("360") || linkaddr.contains("https"))
+					continue;
+				System.out.println(linkaddr + "     " + link.text() + "  RETURN:   " + isWebsiteCanAccess(linkaddr));
 			}
 
 		} catch (Exception e)
